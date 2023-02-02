5acfdddb53130fea985c55899d67bab946c0e068
binnev <byntje@gmail.com>
2023-02-02 14:56:11 +0100

    Added Versions

--- release.py
+++ release.py
@@ -1,9 +1,9 @@
 import shutil
 import subprocess
 from pathlib import Path
-from . import calculator
+import calculator
 
-LIBRARY_NAME = "mkdocs-tutorial"
+LIBRARY_NAME = "mkdocs-ci-cd-sandbox-binnev"
 version = calculator.__version__
 
 
@@ -17,11 +17,32 @@ def cleanup():
             shutil.rmtree(folder)
 
 
+def ask(question: str):
+    response = input(question)
+    if response.lower() not in ["y", "yes"]:
+        raise Exception(f"Action required")
+
+
 if __name__ == "__main__":
+    print(f"Releasing version {version}")
     cleanup()
-    # subprocess.run("python -m build".split())
+    ask(f"Did you create / update the Version changelog for version {version}?\n")
+
+    print("Building package")
+    subprocess.run("python -m build".split())
+    subprocess.run("twine check dist/*".split())
+    print("PyPI test run")
+    subprocess.run("twine upload -r pypitest dist/*".split())
+    ask(f"Does the testpypi output look OK?\n")
+
+    # print("PyPI deploy")
     # subprocess.run("twine upload dist/*".split())
+
+    print("Building docs")
     subprocess.run(f"mike deploy {version}".split())
     subprocess.run(f"mike alias {version} latest --update-aliases".split())
-    subprocess.run(f"mike set-default latest --push".split())
+    process = subprocess.run("mike serve".split())
+    ask("Do the docs look OK?\n")
+
+    # subprocess.run(f"mike set-default latest --push".split())
     cleanup()
---
+++ versions.py
@@ -0,0 +1,4 @@
+from packaging.version import Version
+
+VERSION_1_1_0 = Version("1.1.0")
+VERSION_1_0_0 = Version("1.0.0")
